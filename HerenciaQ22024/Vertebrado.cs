1b511579e7c149e037cfeddcc7253017910cf546
gurdian0614 <ivan.gurdian@outlook.com>
2024-04-22 19:32:34 -0600

    Creando clase hija Vertebrado

---
+++ HerenciaQ22024/Vertebrado.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerenciaQ22024
+{
+    public class Vertebrado : Animal
+    {
+        /// <summary>
+        /// Tipo de Animal
+        /// </summary>
+        public string Tipo {  get; set; }
+
+        /// <summary>
+        /// Valor booleano que nos indica si el animal tiene patas o no
+        /// </summary>
+        public bool TienePatas { get; set; }
+
+        /// <summary>
+        /// Valor booleano que nos indica si el animal es doméstico o no
+        /// </summary>
+        public bool EsDomestico { get; set; } //True: Es domestico // False: No es domestico
+
+        /// <summary>
+        /// Valor booleano que nos indica si el animal es herbívoro o no
+        /// </summary>
+        public bool EsHerbivoro { get; set; } //Bool: Valor booleano: verdadero o falso
+
+        /// <summary>
+        /// Valor booleano que nos indica si el animal es carnívoro o no
+        /// </summary>
+        public bool EsCarnivoro { get; set; }
+
+        /// <summary>
+        /// Imprime cada uno de los atributos de la clase
+        /// </summary>
+        public void Imprimir()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Color: {Color}");
+            Console.WriteLine($"Tamaño: {Tamano}");
+            Console.WriteLine($"Familia: {Familia}");
+            Console.WriteLine($"Tipo: {Tipo}");
+            Console.WriteLine($"Tiene Patas: {(TienePatas ? "Sí" : "No")}");
+            Console.WriteLine($"Es Doméstico: {(EsDomestico ? "Sí" : "No")}");
+            Console.WriteLine($"Es Herbívoro: {(EsHerbivoro ? "Sí" : "No")}");
+            Console.WriteLine($"Es Carnívoro: {(EsCarnivoro ? "Sí" : "No")}");
+        }
+        
+    }
+}
