968e7ee959cd8109eeb7cab23717c30f6ea2754d
gurdian0614 <ivan.gurdian@outlook.com>
2024-04-22 19:32:44 -0600

    Creando Objeto de clase

--- HerenciaQ22024/Program.cs
+++ HerenciaQ22024/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using HerenciaQ22024;
+
+//Creando un objeto
+Vertebrado vertebrado = new Vertebrado();
+vertebrado.Nombre = "Perro";
+vertebrado.Color = "Blanco";
+vertebrado.Tamano = "Pequeño";
+vertebrado.Familia = "Husky";
+vertebrado.Tipo = "Siberiano";
+vertebrado.TienePatas = true;
+vertebrado.EsDomestico = true;
+vertebrado.EsHerbivoro = false;
+vertebrado.EsCarnivoro = true;
+vertebrado.Imprimir();
