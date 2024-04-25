88fdde1b0618f927cf6e2aeb0442117590b2bdc8
gurdian0614 <ivan.gurdian@outlook.com>
2024-04-24 18:37:40 -0600

    Creando clase

---
+++ HerenciaQ22024/Familia.cs
@@ -0,0 +1,16 @@
+﻿
+namespace HerenciaQ22024
+{
+    public class Familia
+    {
+        /// <summary>
+        /// Identificador de familia
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nombre de familia
+        /// </summary>
+        public string Nombre { get; set; }
+    }
+}
