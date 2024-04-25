df01d2fd0e4335ed9a2712a8ec105b141e555e10
gurdian0614 <ivan.gurdian@outlook.com>
2024-04-24 18:37:48 -0600

    Creando clase hija

---
+++ HerenciaQ22024/Invertebrado.cs
@@ -0,0 +1,47 @@
+﻿
+namespace HerenciaQ22024
+{
+    public class Invertebrado : Animal
+    {
+        /// <summary>
+        /// Tipo de Animal
+        /// </summary>
+        public string Tipo { get; set; }
+
+        /// <summary>
+        /// Valor booleano que nos indica si el animal tiene patas o no
+        /// </summary>
+        public bool TienePatas { get; set; }
+
+        /// <summary>
+        /// Valor entero que nos indica el número de patas de un animal
+        /// </summary>
+        public int NumeroPatas { get; set; }
+
+        /// <summary>
+        /// Valor booleano que nos indica si el animal tiene concha o no
+        /// </summary>
+        public bool TieneConcha { get; set; }
+
+        // Constructor
+        public Invertebrado() {
+            TienePatas = false;
+            NumeroPatas = 0;
+        }
+
+        /// <summary>
+        /// Imprime cada uno de los atributos de la clase
+        /// </summary>
+        public void Imprimir()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Color: {Color}");
+            Console.WriteLine($"Tamaño: {Tamano}");
+            Console.WriteLine($"Familia: {Familia.Nombre}");
+            Console.WriteLine($"Tipo: {Tipo}");
+            Console.WriteLine($"Tiene concha: {(TieneConcha ? "Sí" : "No")}");
+            Console.WriteLine($"Tiene Patas: {(TienePatas ? "Sí" : "No")}");
+            Console.WriteLine($"{(TienePatas ? "Número de patas: {NumeroPatas}" : "")}");
+        }
+    }
+}
